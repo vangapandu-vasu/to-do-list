9b3c761b485e2a4f3b8b5abcb43079f2604b15a3
vangapandu-vasu <naiduvasu72@gmail.com>
2025-05-03 18:18:59 +0530

    the draft work.

---
+++ src/routt/mainn.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function mainn() {
+  return (
+    <div>
+        <ol>
+            <li>login</li>
+            <li>home</li>
+            <li>too</li>
+            <li>info</li>
+            <li>contact</li>
+            <li>about</li>
+        </ol>
+    </div>
+  );
+}
+
+
+
+export default mainn;
