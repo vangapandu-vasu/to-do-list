9684c7233232140a9d81e98974fe23b845fbd7d0
vangapandu-vasu <naiduvasu72@gmail.com>
2025-05-03 18:16:33 +0530

    The draft work while learning mern.

---
+++ button/button.jsx
@@ -0,0 +1,14 @@
+import button from "./button.module.css"
+
+function Button(){
+
+
+
+    return(
+        <div>
+            <button className="bu" style={button}>click me</button>
+        </div>
+    );
+}
+
+export default Button
